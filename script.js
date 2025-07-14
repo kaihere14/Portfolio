f109166156ad0fd09338d2f8c12b99617e37cba1
Arman Thakur <yashpalsharma9950@gmail.com>
2025-07-14 13:19:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const bar = document.querySelector(".fa-bars")
+const down = document.querySelector(".down")
+
+let view = false;
+bar.addEventListener("click",()=>{
+    if(view){
+        down.style.display = "flex";
+        view = false
+    }else{
+        down.style.display = "none";
+        view = true;
+    }
+})
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 590) {
+        down.style.display = "none";
+        view = false;
+    }
+});
